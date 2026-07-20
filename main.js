b492e745609f3f552f862147e964d96bb0cbb813
Giga Gabatashvili <g.gabatashvili@G-Gabatashvilis-MacBook-Pro.local>
2026-07-21 00:25:14 +0400

    davaleba_N21

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const userRouter = require("./routes/user.router");
+const authRouter = require("./auth/auth.router");
+const isAuth = require("./middleware/isAuth.middleWare");
+const connectToMongoDB = require("./db/connectToMongo");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+connectToMongoDB();
+
+app.use("/users", isAuth, userRouter);
+app.use("/auth", authRouter);
+
+app.listen(PORT, () => {
+  console.log(`server running on http://localhost:${PORT}`);
+});
